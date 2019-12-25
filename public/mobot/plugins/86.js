a231288d51804176f0ca913b2acc1f3d6cc940e7
Cerbur <1051140642@qq.com>
2019-12-26 00:56:58 +0800

    重构

--- public/mobot/plugins/86.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(e,msg,tag,owner) {return;
-    if(msg.sender.user_id == owner && msg.message.search(/86/) >= 0)
-        return ['86'];
-}
--- public/mobot/plugins/osu.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(e,msg,tag) {
-    if(msg.message.search(/\/\/osu/) != 0) return;
-    return '功能下线'
-}
